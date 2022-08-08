3f6d6f12b1ed298d2687774b10bc25a0f196fb6e
ouroborosno2 <764904497@qq.com>
2022-08-09 00:16:20 +0800

    Initialize

---
+++ src/main/java/com/qgstudio/ProjectMonitorPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.qgstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMonitorPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMonitorPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qgstudio/ProjectMonitorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qgstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMonitorPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
